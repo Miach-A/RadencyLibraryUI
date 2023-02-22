648318ef885df6604a47d5b5e20ba9a7e3ae4680
Miach-A <ovmiach@gmail.com>
2023-02-22 18:39:54 +0200

    edit-book component

--- src/app/modules/angular-material.module.ts
+++ src/app/modules/angular-material.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTabsModule} from '@angular/material/tabs';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, MatDividerModule, MatCardModule, MatInputModule],
-  exports: [MatDividerModule, MatCardModule, MatInputModule],
+  imports: [CommonModule, MatDividerModule, MatCardModule, MatInputModule,MatButtonModule,MatTabsModule],
+  exports: [MatDividerModule, MatCardModule, MatInputModule,MatButtonModule,MatTabsModule],
 })
 export class AngularMaterialModule {}
--- src/app/services/edit-book-state.service.ts
+++ src/app/services/edit-book-state.service.ts
@@ -1,13 +1,21 @@
 import { Injectable } from '@angular/core';
 
+enum Action{
+  Add = "Add",
+  Edit = "Edit"
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class EditBookStateService {
 
   public title:string;
-  
+  public action:Action;
+
   constructor() {
     this.title = "Add Book";
+    this.action = Action.Add;
   }
 }
+
