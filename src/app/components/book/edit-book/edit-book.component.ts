5e7607eb51654e1fbabbca7bc86866175873065a
Miach-A <ovmiach@gmail.com>
2023-02-23 19:40:15 +0200

    edit-book

--- src/app/components/book/edit-book/edit-book.component.ts
+++ src/app/components/book/edit-book/edit-book.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { BackendService } from 'src/app/services/backend.service';
 import { EditBookStateService } from 'src/app/services/edit-book-state.service';
 
 @Component({
@@ -8,11 +11,27 @@ import { EditBookStateService } from 'src/app/services/edit-book-state.service';
 })
 export class EditBookComponent implements OnInit {
 
+  public editForm!:FormGroup;
+  private _subscription:Subscription = new Subscription();
+
   constructor(
-    public editBookStateService : EditBookStateService
+    public editBookStateService : EditBookStateService,
+    private backendService:BackendService
   ) { }
 
   ngOnInit(): void {
+    this.editForm = new FormGroup({
+      title: new FormControl('',[Validators.required,Validators.minLength(3),Validators.maxLength(128)]),
+      Cover: new FormControl('',[Validators.required]),
+      Genre: new FormControl('',[Validators.required]),
+      Author: new FormControl('',[Validators.required]),
+      Content: new FormControl('',[Validators.required])
+    });
+  }
+
+  public Submit(){
+    this._subscription.unsubscribe();
+    this._subscription = this.backendService.put('user',this.editForm.value).subscribe();
   }
 
 }
